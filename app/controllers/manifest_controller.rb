b2a05fbb8a7cda8b398a796800fe38eb017d1a9e
Yekkanti Kishore Kumar <kishoreyekkanti@gmail.com>
2012-04-07 16:56:47 +0530

    removed the versio update of cache manifest

--- app/controllers/manifest_controller.rb
+++ app/controllers/manifest_controller.rb
@@ -1,8 +1,6 @@
 class ManifestController < ApplicationController
 
   def show
-    time = Time.now
-    @updated_time = time.year.to_s+time.month.to_s+time.day.to_s+time.hour.to_s+time.min.to_s
     render content_type: "text/cache-manifest", layout: false
   end
 end
